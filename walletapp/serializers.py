709f8a2be4c1f881b40e772f772b6beabd2fe5bf
Sakib Raza <sakib@sakib.raza>
2022-07-06 11:02:32 +0530

    Initial Commit

--- walletapp/serializers.py
+++ walletapp/serializers.py
@@ -1,12 +1,20 @@
+from django.forms import ValidationError
 from requests import request
 from rest_framework import serializers
 from .models import Wallet, WalletType, User
+from rest_framework.validators import UniqueTogetherValidator
 
 
 class WalletSerializer(serializers.ModelSerializer):
-
+                 
     class Meta:
         model = Wallet
         fields = ['pay_type', 'user', 'balance']
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Wallet.objects.all(),
+                fields=['pay_type', 'user']
+            )
+        ]
     
-        
+    
